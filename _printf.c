b8568c2fac1cc77a2d3cbf407411a117b7e4b227
michelalejo <michelmolina65@gmail.com>
2019-10-28 04:24:57 +0000

    printf-1.0

--- _printf.c
+++ _printf.c
@@ -1,9 +1,6 @@
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include "holberton.h"
-
-#include <stdio.h>
 
 /**
  * _printf - function that prints formated text in standard output.
@@ -15,57 +12,49 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j = 0, temp_d = 0, count_conversion = 0, chars_printed = 0;
-	char *buffer = NULL, *temp_s = NULL;
+	int i = 0, size = 0;
+	char b[] = {'\0', '\0'};
+	char *temp_c = b, *temp_s;
 
 	if (format == NULL)
 		return (0);
 
-	buffer = malloc(buffer_size * sizeof(char));
-	if (buffer == NULL)
-		return (0);
-	temp_s = malloc(buffer_size * sizeof(char));
-	if (temp_s == NULL)
-		return (0);
-
-	free_temp(temp_s);
-	free_temp(buffer);
 	va_start(args, format);
 
-	for (i = 0, j = 0; *(format + i) != '\0'; i++, j++)
+	for (i = 0; *(format + i) != '\0'; i++)
 	{
 		if (*(format + i) == '%')
 		{
-			count_conversion = count_id(format + i);
-			if (count_conversion == 0)
+			switch (*(format + i + 1))
+			{
+			case 'c':
+				*(temp_c) = va_arg(args, int);
+				if (temp_c)
+					write(1, temp_c, 1);
 				break;
-			/*char *temp = checkformat(format + i, args);*/
-			if (*(format + i + count_conversion) == 'c')
-			{
-				free_temp(temp_s);
-				temp_d = va_arg(args, int);	/*if (temp_d == 0)] ????*/
-				*(temp_s + 0) = temp_d;
-				*(temp_s + 1) = '\0';
-				temp_s = concat(buffer, temp_s);
-				if (temp_s != NULL)
-					buffer = temp_s;
-			}
-			if (*(format + i + count_conversion) == 's')
-			{
-				free_temp(temp_s);
+			case 's':
 				temp_s = va_arg(args, char *);
-				j += size_temp(temp_s) - 1;
-				temp_s = concat(buffer, temp_s);
-				if (temp_s != NULL)
-					buffer = temp_s;
+				if (temp_s)
+				{
+					size = size_temp(temp_s);
+					write(1, temp_s, size);
+				}
+				else
+					write(1, "(null)", 6);
+				break;
+			default:
+				*temp_c = *(format + i + 1);
+				write(1, temp_c, 1);
 			}
-			i += count_conversion;
+			i++;
 		}
 		else
-			*(buffer + j) = *(format + i);/* Adds a char to buffer */
+		{
+			*temp_c = *(format + i);
+			write(1, temp_c, 1);
+		}
 	}
-	chars_printed = write(1, buffer, j);
+
 	va_end(args);
-	free(buffer);
-	return (chars_printed);
+	return (i);
 }
